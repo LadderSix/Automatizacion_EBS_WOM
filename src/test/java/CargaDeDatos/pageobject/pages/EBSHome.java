ab62e737c15146162909e86d9f7faebef74cb654
Matias Rojas <Matias.Rojas@tsoftglobal.com>
2021-08-13 15:25:26 -0400

    nuevo repo para wom

---
+++ src/test/java/CargaDeDatos/pageobject/pages/EBSHome.java
@@ -0,0 +1,17 @@
+package CargaDeDatos.pageobject.pages;
+
+import CargaDeDatos.pageobject.base.SeleniumBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class EBSHome extends SeleniumBase {
+
+    public EBSHome(WebDriver driver){
+        super(driver);
+    }
+
+    //repositorio objetos JetSmartHomePAge:
+
+
+
+}
---
+++ src/test/java/testcases/CargaDeEquipoSimcard.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import CargaDeDatos.pageobject.pages.EBSHome;
+
+import org.junit.Test;
+
+
+
+public class CargaDeEquipoSimcard extends TestBase {
+
+    protected  EBSHome home;
+
+    @Test
+    public void CargaDeEquipos(){
+        home = new EBSHome(driver);
+        home.goToUrl("http://junin.novaltepre.corp:8003/");
+    }
+
+}
